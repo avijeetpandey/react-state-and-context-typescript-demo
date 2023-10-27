9447e4615c717a1a587000d4b1c57cf6f428d036
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-10-27 22:13:41 +0530

    Add: start and stop timer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,15 @@ export default function Header() {
   return (
     <header>
       <h1>ReactTimer</h1>
-
-      <Button>{timersContext.isRunning ? "Stop" : "Start"} Timers</Button>
+      <Button
+        onClick={() => {
+          timersContext.isRunning
+            ? timersContext.stopTimers()
+            : timersContext.startTimers();
+        }}
+      >
+        {timersContext.isRunning ? "Stop" : "Start"} Timers
+      </Button>
     </header>
   );
 }
