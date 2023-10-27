95536b48da6ce594244318b59a4ae70bb3e13e89
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-10-27 19:03:46 +0530

    Add: context component

---
+++ src/store/timer-context.tsx
@@ -0,0 +1,42 @@
+import React, { type ReactNode, createContext } from "react";
+
+type Timer = {
+  name: string;
+  duration: string;
+};
+
+type TimerState = {
+  isRunning: boolean;
+  timers: Array<Timer>;
+};
+
+type TimersContextValue = TimerState & {
+  addTimer: (timerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
+
+const TimersContext = createContext<TimersContextValue | null>(null);
+
+// Provider for the context API
+type TimerContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimerContextProvider({
+  children
+}: TimerContextProviderProps) {
+  const ctx: TimersContextValue = {
+    isRunning: false,
+    timers: [],
+    addTimer(timerData) {
+      console.log(timerData);
+    },
+    startTimers() {},
+    stopTimers() {}
+  };
+
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
